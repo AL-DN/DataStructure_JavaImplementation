073bc071a019ab8b5ebd3154172d01ba876a7e0f
Sahi <axs9247@miami.edu>
2024-03-22 13:15:19 -0400

    replaceAt() added.

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+// Author: Alden Sahi
+// Date: 03/20/2024
+// Project: LinkedList Implementation in Java
+// Project Description: We will create a LinkedList in java to dynamically store data.
+// This is for practice to help reinforce emy understanding of data structures.
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        /* java built-in linked list
+        LinkedList list = new LinkedList();
+        list.add(5);
+        list.add(0,12);
+        System.out.println(list);
+        */
+        LinkedList list = new LinkedList();
+        list.insertF(8);
+        list.insertF(9);
+        list.insertF(10);
+        list.replaceAt(0,11);
+        list.show();
+
+    }
+
+}
