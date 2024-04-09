c54bc24098ca6dec50afc52662c0f656b311f88e
Sahi <axs9247@miami.edu>
2024-04-08 23:03:59 -0400

    insert and show methods added with respective recursive functions

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -10,26 +10,56 @@ class Node {
     }
 }
 
-
 public class BinarySearchTree {
 
-    Node root;
+    Node root = null; // ptr to root node
 
     public void insert(int data) {
-        // Create node for insertion
-        Node node = new Node(data);
 
-        // if the root node is pointing to nothing the first to be inserted will be root
-        if(root.left == null && root.right == null) {
+        root = insertRecursion(root, data);
+
+    }
+
+    public Node insertRecursion(Node root, int data) {
+
+        // we can still have a root with this code "if (root.left == null && root.right
+        // == null)"
+        // we need to check if the ptr to root is null
+        if (root == null) {
             root = new Node(data);
         }
+        // if data is less than the root
         else if (data < root.data) {
-            root.left = node;
+            // now we will need to update root to make a subtree
+            root.left = insertRecursion(root.left, data);
+        } else if (data > root.data) {
+            root.right = insertRecursion(root.right, data);
         }
-        else if (data > root.data) {
-            root.right = node;
-        
-        
+
+        // at the end of these if else statements it will find a null/vacant spot
+
+        return root;
+
     }
-    
+
+    // InOrder: left root right
+    // PreOrder: root left right
+    // PostOrder: left right root
+    public void inOrder() {
+        inOrderRecursion(root);
+    }
+
+    public void inOrderRecursion(Node root) {
+
+        // INORDER IMPLEMENTATION
+        if (root != null) {
+            // this will recursively call itself until it finds smallest value
+            // can easily be rearranged to fit printing style needed
+            inOrderRecursion(root.left);
+            System.out.print(root.data + " ");
+            inOrderRecursion(root.right);
+        }
+
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -3,41 +3,51 @@
 // Project: Data Structure Implementation in Java
 // Project Description: We will make our own java implementation of LinkedList, Stack, and Queue.
 
-
 public class Main {
     public static void main(String[] args) {
-        /* LinkedList list = new LinkedList();
-        list.show();
-        list.insertAtStart(3);
-        list.show();
-        list.insertAtStart(2);
-        list.show();
-        list.insertAtStart(1);
-        list.show();
-        list.insertAtEnd(4);
-        list.show();
-        list.replaceAt(2,2);
-        list.show();
-        list.insertAtIndex(1,4);
-        list.show();
-        list.countNodes(); */
+        /*
+         * LinkedList list = new LinkedList();
+         * list.show();
+         * list.insertAtStart(3);
+         * list.show();
+         * list.insertAtStart(2);
+         * list.show();
+         * list.insertAtStart(1);
+         * list.show();
+         * list.insertAtEnd(4);
+         * list.show();
+         * list.replaceAt(2,2);
+         * list.show();
+         * list.insertAtIndex(1,4);
+         * list.show();
+         * list.countNodes();
+         */
 
-        /* Stack stacky = new Stack();
-        stacky.push(8);
-        stacky.push(2);
-        stacky.show();
-        System.out.println(stacky.pop());
-        System.out.println(stacky.peek());
-        System.out.println(stacky.isEmpty()); */
+        /*
+         * Stack stacky = new Stack();
+         * stacky.push(8);
+         * stacky.push(2);
+         * stacky.show();
+         * System.out.println(stacky.pop());
+         * System.out.println(stacky.peek());
+         * System.out.println(stacky.isEmpty());
+         */
 
-        /*Queue queue = new Queue();
-        queue.enqueue(2);
-        queue.enqueue(3);
-        queue.enqueue(4);
-        queue.show(); */
+        /*
+         * Queue queue = new Queue();
+         * queue.enqueue(2);
+         * queue.enqueue(3);
+         * queue.enqueue(4);
+         * queue.show();
+         */
 
-        BinaryTree tree = new BinaryTree();
-        tree.insert();
+        BinarySearchTree tree = new BinarySearchTree();
+        tree.insert(10);
+        tree.insert(2);
+        tree.insert(6);
+        tree.insert(4);
+        tree.insert(8);
+        tree.inOrder();
 
     }
 
